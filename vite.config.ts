5a537fe2b36cad7fc25586aff738c30e54ba23e7
mabbas <mehwish.abbas@init.de>
2022-07-11 04:26:47 +0200

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/wallcontroller': {
+        target: 'http://localhost:8080'
+      }
+    }
+  },
+  base: "/wallcontroller/",
+  build: {
+    outDir: "../src/main/resources/static/wallcontroller",
+    emptyOutDir: true
+  }
 })
